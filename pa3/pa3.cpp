e820fcad45d6f863875ab6f1c96879bb77a48aed
Rohan Kharwadkar <rohankharwadkar@ufl.edu>
2018-04-07 22:08:59 -0400

    first commit

--- pa3/pa3.cpp
+++ pa3/pa3.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 
 int main(){
+
+
+	cout << "Please enter the name of the input file:" << endl;
+	string fileName;
+	cin>>fileName;
+
+	string inputTxt;
+	ifstream myfile(fileName);
+	if(!myfile){
+		return -1;
+	}
+	while(getline(myfile, inputTxt)){
+		cout << inputTxt << endl;
+	}
+	myfile.close();
+
+	int inputLength = inputTxt.length();
+
+	for(int i = 0; i < inputLength; i++){
+		if (inputTxt.at(i) == 'F' && inputTxt.at(i+1) == 'O' && inputTxt.at(i+2) == 'R'){
+			cout << "wow a for loop" << endl;
+		}
+	}
+
 	return 0;
-}
+} 
